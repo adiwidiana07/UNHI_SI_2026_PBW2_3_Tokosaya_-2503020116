7a0bcdba7abc6c7ddef63a32f3f4faaf4fbb0b6b
adiwidiana@gmail.com <putuadiwidiana@gmail.com>
2026-09-18 09:15:10 +0800

    refactor: merapikan struktur route dengan memindahkan logika home, kontak, dan produk ke controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\HalamanController;
+use App\Http\Controllers\ProdukController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('Beranda');
-});
+Route::get('/', [HalamanController::class, 'home'])->name('home');
+Route::get('/kontak', [HalamanController::class, 'kontak'])->name('kontak');
+
+Route::get('/produk', [ProdukController::class, 'index'])->name('produk.index');
+Route::get('/produk/{id}', [ProdukController::class, 'show'])->name('produk.show');
 
 Route::get('/admin', function () {
     return view('admin.dashboard');
